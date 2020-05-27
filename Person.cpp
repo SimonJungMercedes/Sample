52efb5d505c9207cbbd7ec0d218dbffdd94e764c
Simon Jung <jung@IT-AUSBILDUNG.local>
2020-05-27 09:23:25 +0200

    Hello

--- Person.cpp
+++ Person.cpp
@@ -1,42 +1,29 @@
+/*
+ * Person.cpp
+ *
+ *  Created on: 20-May-2020
+ *      Author: ashishsharma
+ */
+
+
 #include <iostream>
-#include <string>
+#include<string>
+
 using namespace std;
 
-class Person
-	{
-		public:
-			string name, adress;
-			char gender;
-			int mobileNumber, age;
-	
-		public:
-		string get_name(){return name;}
-			
-		public:
-		int get_age(){return age;}
-			
-		public:
-		char get_gender(){return gender;}
-			
-		public:
-		int get_mobileNumber(){return mobileNumber;}
-		
-		public:
-		string get_adress(){return adress;}
-		
-		public:
-		string set_name(){name=newName;}
-			
-		public:
-		int set_age(){age=newAge;}
-			
-		public:
-		char set_gender(){gender=newGender;}
-			
-		public:
-		int set_mobileNumber(){mobileNumber=newMobileNumber;}
-		
-		public:
-		string set_adress(){adress=newAdress;}
-				
+class Person {
+
+private:
+	string name;
+
+public:
+	void set_name(string name) {
+		this->name = name;
 	}
+
+	string get_name() {
+		return name;
+	}
+	
+
+};
